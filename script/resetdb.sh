f9deb2ede8b335a672434800f399d9dd73985a7c
planetskht <planetskht@users.noreply.github.com>
2017-02-18 18:39:27 +0530

    update scripts

--- script/resetdb.sh
+++ script/resetdb.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+echo "Stop Apache Server"
+sudo service httpd stop
+
 rm -rf public/uploads/attachment
 echo "db:drop.."
 rake db:drop
@@ -8,3 +11,6 @@ echo "db:migrate..."
 rake db:migrate
 echo "db:seed..."
 rake db:seed
+
+echo "Restart Apache Server"
+sudo service httpd restart
